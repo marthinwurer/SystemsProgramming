ea49ae4f35f8a354f92bab4c82a55375b521b648
pedgeon <b.ringey@gmail.com>
2017-04-04 00:36:56 -0400

    Merge config branch commit de7a0f0e45af6dd94f5794f7fd147b9fbd933dd1

---
+++ mk/constants.mk
@@ -0,0 +1,10 @@
+# mk/constants.mk
+# Makefile for constant variable declarations
+
+
+# Directories
+
+INC_DIR := include
+
+BUILDIMAGE_DIR := BuildImage
+BUILDIMAGE := $(BUILDIMAGE_DIR)/BuildImage
---
+++ mk/kern.mk
@@ -0,0 +1,41 @@
+# mk/kern.mk
+# Makefile for the kern and baseline folders
+
+U_C_OBJ := main.o vga13/vga13.o
+U_C_OBJ := $(addprefix $(BUILD_DIR)/kern/,$(U_C_OBJ))
+
+U_S_OBJ :=
+U_S_OBJ := $(addprefix $(BUILD_DIR)/kern/,$(U_S_OBJ))
+
+
+# Binary/source file for system bootstrap code
+
+BOOT_OBJ := bootstrap.b
+BOOT_OBJ := $(addprefix $(BUILD_DIR)/kern/baseline/,$(BOOT_OBJ))
+
+# Assembly language object/source files
+
+S_OBJ := startup.o isr_stubs.o 
+S_OBJ := $(addprefix $(BUILD_DIR)/kern/baseline/,$(S_OBJ))
+
+# C object/source files
+
+C_OBJ := c_io.o support.o
+C_OBJ := $(addprefix $(BUILD_DIR)/kern/baseline/,$(C_OBJ))
+
+# Collections of files
+
+KERN_OBJECTS = $(S_OBJ) $(C_OBJ) $(U_C_OBJ) $(U_S_OBJ)
+
+$(BUILD_DIR)/prog.out: $(KERN_OBJECTS)
+	$(LD) $(LDFLAGS) -o $@ $(OBJECTS)
+
+$(BUILD_DIR)/prog.o: $(KERN_OBJECTS)
+	$(LD) $(LDFLAGS) -o $@ -Ttext 0x10000 $+ $(U_LIBS)
+
+$(BUILD_DIR)/prog.b: $(BUILD_DIR)/prog.o
+	$(LD) $(LDFLAGS) -o $@ -s --oformat binary -Ttext 0x10000 $<
+
+# the -MD flags create dependency files when compiling, so we include them
+# here (the - ignores errors if the file does not exist)
+-include $(KERN_OBJECTS:.o=.d)
