885b1dbdabad4895692368dea88f1be930e3b2b9
pedgeon <b.ringey@gmail.com>
2017-04-23 10:14:49 -0400

    Added structure typedefs for VBE 3.0 and VBE 2.0 ModeInfo blocks Changed VBEModeInfo typedef to a union of VBE3ModeInfo and VBE2ModeInfo

---
+++ include/kern/vesa/VBE2ModeInfo.h
@@ -0,0 +1,49 @@
+#ifndef _KERN_VESA_VBE2MODEINFO_H
+#define _KERN_VESA_VBE2MODEINFO_H
+
+//
+// VESA BIOS 2.0 Extensions Mode information block structure
+//
+struct VBE2ModeInfo_s {
+	// Mandatory information
+	uint16_t ModeAttributes;
+	uint8_t  WinAAttributes;
+	uint8_t  WinBAttributes;
+	uint16_t WinGranularity;
+	uint16_t WinSize;
+	uint16_t WinASegment;
+	uint16_t WinBSegment;
+	uint32_t WinFuncPtr;
+	uint16_t BytesPerScanLine;
+	// VBE 1.2 and above
+	uint16_t XResolution;
+	uint16_t YResolution;
+	uint8_t  XCharSize;
+	uint8_t  YCharSize;
+	uint8_t  NumberOfPlanes;
+	uint8_t  BitsPerPixel;
+	uint8_t  NumberOfBanks;
+	uint8_t  MemoryModel;
+	uint8_t  BankSize;
+	uint8_t  NumberOfImagePages;
+	uint8_t  reserved0;
+	uint8_t  RedMaskSize;
+	uint8_t  RedFieldPosition;
+	uint8_t  GreenMaskSize;
+	uint8_t  GreenFieldPosition;
+	uint8_t  BlueMaskSize;
+	uint8_t  BlueFieldPosition;
+	uint8_t  RvsdMaskSize;
+	uint8_t  RvsdFieldPosition;
+	uint8_t  DirectColorModeInfo;
+	// VBE 2.0 and above
+	uint32_t PhysBasePtr;
+	uint32_t OffScreenMemOffset;
+	uint16_t OffScreenMemSize;
+	uint8_t reserved1[206];
+} __attribute__((packed));
+
+typedef struct VBE2ModeInfo_s VBE2ModeInfo;
+
+
+#endif
--- include/kern/vesa/VBEModeInfo.h
+++ include/kern/vesa/VBEModeInfo.h
@@ -1,62 +1,13 @@
 #ifndef _KERN_VESA_VBEMODEINFO_H
 #define _KERN_VESA_VBEMODEINFO_H
 
-//
-// VESA BIOS Extensions Mode information block structure
-//
-struct VBEModeInfo_s {
-	// Mandatory information
-	uint16_t ModeAttributes;
-	uint8_t  WinAAttributes;
-	uint8_t  WinBAttributes;
-	uint16_t WinGranularity;
-	uint16_t WinSize;
-	uint16_t WinASegment;
-	uint16_t WinBSegment;
-	uint32_t WinFuncPtr;
-	uint16_t BytesPerScanLine;
-	// VBE 1.2 and above
-	uint16_t XResolution;
-	uint16_t YResolution;
-	uint8_t  XCharSize;
-	uint8_t  YCharSize;
-	uint8_t  NumberOfPlanes;
-	uint8_t  BitsPerPixel;
-	uint8_t  NumberOfBanks;
-	uint8_t  MemoryModel;
-	uint8_t  BankSize;
-	uint8_t  NumberOfImagePages;
-	uint8_t  reserved0;
-	uint8_t  RedMaskSize;
-	uint8_t  RedFieldPosition;
-	uint8_t  GreenMaskSize;
-	uint8_t  GreenFieldPosition;
-	uint8_t  BlueMaskSize;
-	uint8_t  BlueFieldPosition;
-	uint8_t  RvsdMaskSize;
-	uint8_t  RvsdFieldPosition;
-	uint8_t  DirectColorModeInfo;
-	// VBE 2.0 and above
-	uint32_t PhysBasePtr;
-	uint32_t reserved1;
-	uint16_t reserved2;
-	// VBE 3.0 and above
-	uint16_t LinBytesPerScanLine;
-	uint8_t  BnkNumberOfImagePages;
-	uint8_t  LinNumberOfImagePages;
-	uint8_t  LinRedMaskSize;
-	uint8_t  LinRedFieldPosition;
-	uint8_t  LinGreenMaskSize;
-	uint8_t  LinGreenFieldPosition;
-	uint8_t  LinBlueMaskSize;
-	uint8_t  LinBlueFieldPosition;
-	uint8_t  LinRsvdMaskSize;
-	uint8_t  LinRsvdFieldPosition;
-	uint32_t MaxPixelClock;
-	uint8_t reserved3[189];
-} __attribute__((packed));
+#include <kern/vesa/VBE2ModeInfo.h>
+#include <kern/vesa/VBE3ModeInfo.h>
 
-typedef struct VBEModeInfo_s VBEModeInfo;
+typedef union VBEModeInfo_u {
+	VBE2ModeInfo v2;
+	VBE3ModeInfo v3;
+} VBEModeInfo;
 
 
 #endif
