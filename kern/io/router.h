9ecc0b8963d4f027bcdcdd2a18c976210791c03c
Nathan Castle <nathan.castle@outlook.com>
2017-04-23 04:00:52 -0400

    improved documentation for router; adherance to doxy format

--- kern/io/router.h
+++ kern/io/router.h
@@ -11,68 +11,50 @@
 #include "message.h"
 
 /**
- * @summary: allocates space for an object and returns handle
- * @returns status_t
- *          E_SUCCESS
- *          E_NO_MORE_HANDLES
- * @in obj_type - type of object to prototype
- * @out phandle - object handle
- * */
+ * \brief allocates space for an object and returns handle
+ * \return status_t: E_SUCCESS or E_NO_MORE_HANDLES
+ * \param obj_type [in] type of object to prototype
+ * \param  phandle [in] object handle
+ */
 status_t IO_PROTOTYPE(IO_OBJ_TYPE type, PIOHANDLE handle);
 
 /**
- * @summary: updates a property of an IO object
- * @returns status_t
- *          E_SUCCESS
- *          E_BAD_HANDLE
- *          E_BAD_PROPERTY
- *          E_INVALID_VALUE
- *          E_LESS_DATA - less data than specified in length
- *          E_LOCKED - object has been locked and can't be updated
- * @in: handle - handle to object being updated
- * @in: property - property to update
- * @in: value - value to set
- * @in: length - length of value to set
- * @out: length - length of value written
+ * \brief updates a property of an IO object
+ * \return status_t: E_SUCCESS, E_BAD_HANDLE, E_BAD_PROPERTY,
+ *          E_INVALID_VALUE, E_LESS_DATA, E_LOCKED
+ * \param handle [in] handle to object being updated
+ * \param property [in] property to update
+ * \param value [in] value to set
+ * \param length [in] length of value to set [out] length of value written
  * */
 status_t IO_UPDATE(IOHANDLE handle, IOPROP property, void* value, PBSIZE length);
 
 /**
- * @summary: deletes an object; interpreted as cancel for IO Messages
- * @returns status_t
- *          E_SUCCESS
- *          E_BAD_HANDLE
- * @in: handle - object to delete
+ * \brief deletes an object; interpreted as cancel for IO Messages
+ * \return status_t: E_SUCCESS or E_BAD_HANDLE
+ * \param handle [in] object to delete
  * */
 status_t IO_DELETE(IOHANDLE handle);
 
 /**
- * @summary: for use with IO Message; executes IO pipeline
- * @returns status_t
- *          E_SUCCESS
- *          E_BAD_HANDLE
- *          **other IO pipeline results here **
- * @in: handle
+ * \brief for use with IO Message; executes IO pipeline
+ * \return status_t: E_SUCCESS or E_BAD_HANDLE
+ * \param handle [in] handle to IO Message representing request
  * */
 status_t IO_EXECUTE(IOHANDLE handle);
 
 /**
- * @summary: enumerates through the objects of a particular type
- * @returns status_t
- *          E_SUCCESS
- *          E_OUT_OF_BOUNDS
- * @in: type of object to iterate for
- * @in: index of object (in view of iteration)
- * @out: object - handle to object that was found
+ * \brief enumerates through the objects of a particular type
+ * \return status_t: E_SUCCESS or E_OUT_OF_BOUNDS
+ * \param type [in] type of object to iterate for
+ * \param index [in] index of object (in view of iteration)
+ * \param object [out] handle to object that was found
  * */
 status_t IO_ENUMERATE(IO_OBJ_TYPE type, int index, PIOHANDLE object);
 
 /**
- * @summary: locks an object from further modification
- * @returns status_t
- *          E_SUCCESS
- *          E_BAD_HANDLE
- *          E_NOT_LOCKABLE
- * @in: handle
+ * \brief locks an object from further modification
+ * \return status_t: E_SUCCESS, E_BAD_HANDLE, or E_NOT_LOCKABLE
+ * \param handle [in] handle to object to be locked
  * */
 status_t IO_LOCK(IOHANDLE handle);
