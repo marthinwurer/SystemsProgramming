bad3385edcc1c9e2381b62418d113f523eabf75f
pedgeon <b.ringey@gmail.com>
2017-04-27 01:57:47 -0400

    Added early video initialization function prototypes and stubs

---
+++ include/kern/early/video.h
@@ -0,0 +1,35 @@
+#ifndef _KERN_EARLY_VIDEO_H
+#define _KERN_EARLY_VIDEO_H
+
+
+#include <kern/video/VideoMode.h>
+
+/**
+ * @brief Initializes all global variables in the video module.
+ *
+ * Global variables in the video module that contain information obtainable
+ * only in early init are set by this function. The VBE Controller information
+ * as well as the EDID record are obtained and set by this function.
+ */
+int video_early_init(void);
+
+
+/**
+ * @brief Determines the best available mode using VBE/EDID information.
+ *
+ * This function uses the variables set by video_early_init to determine the
+ * best available video mode. There are a few possibilities for the mode
+ * selected and are listed below in order from best to worst:
+ *
+ *    - Native mode: best resolution supported by monitor
+ *    - Standard timing: one of the standard resolutions supported by monitor
+ *    - Established: one of the standard resolutions by VESA
+ *    - "Safe": a resolution that generally all monitors support
+ *    - Legacy VGA: VGA Mode 0x13 (320x200x8)
+ *
+ */
+int video_early_bestMode(VideoMode *mode);
+
+
+
+#endif
---
+++ kern/early/video.c
@@ -0,0 +1,26 @@
+/*
+** File: kern/early/video.c
+**
+** Author: Brennan Ringey (bjr1251)
+**
+** Contains function implementations defined in include/kern/early/video.h
+**
+** The functions defined here are for early initialization for the video
+** module. 
+**
+*/
+
+
+#include <kern/early/video.h>
+
+
+int video_early_init(void) {
+
+	return E_VIDEO_SUCCESS;
+}
+
+
+int video_early_bestMode(VideoMode *mode) {
+
+	return E_VIDEO_SUCCESS;
+}
