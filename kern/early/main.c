7af87bc659007c186bc2dc9d2e3705cea12a6bff
pedgeon <b.ringey@gmail.com>
2017-05-01 23:21:40 -0400

    Removed font drawing demo in main function

--- kern/early/main.c
+++ kern/early/main.c
@@ -17,35 +17,14 @@
 #define EXIT_SUCCESS 0
 
 
-void __putchar(VideoFb *fb, int x, int y, VideoCol fg, VideoCol bg, char c) {
-
-	uint8_t *glyph = (uint8_t*)(0x3C00 + (c * 16));
-	VideoCol rowBuf[8];
-	uint8_t row;
-
-	for (int i = 0; i != 16; ++i) {
-		row = glyph[i];
-		for (int j = 0; j != 8; ++j) {
-			if ((row >> (7 - j)) & 1) {
-				rowBuf[j] = fg;
-			} else {
-				rowBuf[j] = bg;
-			}
-		}
-		fb_putcols(fb, x, y + i, 8, rowBuf);
-	}
-
-}
-
 //
 // Main function for the early initialization routine. Any needed BIOS function
 // calls should be done here while storing the results somewhere the kernel can
-// access it. Note that you cannot use c_io since, like the bootstrap, it is
-// not linked with that code. Any errors that occur here are to be handled 
-// by the kernel (unless the error is unrecoverable, then just halt).
+// access it. Any errors that occur here are to be handled by the kernel
+// (unless the error is unrecoverable, then return nonzero).
 //
 // Nothing has been setup yet, so there should be no problems setting things
-// up using the protected-to-real mode int function.
+// up using the protected-to-real mode int32 function.
 //
 int main(void) {
 
@@ -82,36 +61,6 @@ int main(void) {
 		c_printf("Failed to switch mode. Code %d\n", errorcode);
 		return EXIT_VIDEO_ERROR;
 	}
-
-	// white-on-purple test
-
-	VideoCol white = color_getColor(mode.fb.colorspace, 255, 255, 255);
-	VideoCol purple = color_getColor(mode.fb.colorspace, 75, 0, 130);
-
-	fb_clear(&mode.fb, purple);
-
-	PaintContext ctx;
-	ctx.drawCol = white;
-	ctx.fillCol = purple;
-	ctx.font.bytesPerGlyph = 16;
-	ctx.font.height = 16;
-	ctx.font.width = 8;
-	ctx.font.glyphs = 256;
-	ctx.font.glyphMap = VIDEO_FONTSET;
-	ctx.fb = &mode.fb;
-
-	Point p = { .x = 0, .y = 0};
-	graphics_drawString(&ctx, &p, "TEST string! shitlord.");
-
-	// test draw character using fontset at 0x3C00
-	// int x, y;
-	// for (int i = 0; i != 256; ++i) {
-	// 	x = (i % 32) * 8;
-	// 	y = (i / 32) * 16;
-	// 	__putchar(&mode.fb, 16 + x, 16 + y, white, purple, (char)i);
-	// }
-
-	//__putchar(&mode.fb, 10, 10, white, purple, 'B');
 
 	__asm("hlt");
 
